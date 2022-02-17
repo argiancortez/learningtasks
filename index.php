81f02c5afa42fce4325b5dbb64d22cfb2bf43d7f
argiancortez <99863214+argiancortez@users.noreply.github.com>
2022-02-17 15:02:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <title>My Home Page</title>
 </head>
 <body>
-<h1> Welcome to My Website! I am Argian Rivera Cortez! 
+<h1> Welcome to My Website! I am Argian Rivera Cortez! <\h1>
 <ul>
 <li><a href="index.php">HOME</a></li>
 <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
